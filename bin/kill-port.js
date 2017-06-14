637645b17726c2edd021cf68b154d73e563c9e0e
changfeng.liu <361065934@qq.com>
2017-06-14 22:19:00 +0800

    完善kill-port模块

---
+++ bin/kill-port.js
@@ -0,0 +1,13 @@
+#!/usr/bin/env node
+let logger = require('../lib/util/logger')
+let args = process.argv
+let port = args[2]
+
+if(port == '-h' || port == '-help' || port == '--help'){
+  logger.sep()
+  logger.help('Usage: mcfe-kp port')
+  logger.help('Example: mcfe-kp 8080')
+  logger.sep()
+} else {
+  require('../lib/kill-port/index')(port)
+}
