f4cd4732496a7d0a57ee824ddb036bf645dd3647
changfeng.liu <361065934@qq.com>
2017-06-15 02:22:59 +0800

    完善http-server

--- NEXT.md
+++ NEXT.md
@@ -1,3 +1,3 @@
 1. 完善命令行帮助提示
-2. 完善http-server的host与proxy功能
+2. 完善http-server的host与proxy功能, 使用本地的php服务处理php文件
 3. 完善module-chart模块
--- bin/http-server.js
+++ bin/http-server.js
@@ -8,57 +8,69 @@ var os       = require('os'),
   logger     = require('../lib/util/logger'),
   argv       = require('../lib/http-server/cmd').argv;
 
-if (!argv['port']) {
+if (!argv['port'] || argv['port'] == 8080) {
   portfinder.basePort = 8080;
+  //基于8080找一个可用的端口.
   portfinder.getPort(function (err, port) {
-    if (err) { throw err; }
-    listen(port);
+    if (err) {
+      throw err;
+    } else {
+      listen(port);
+    }
   });
 } else {
   listen(argv['port']);
 }
 
 function listen(port) {
-  var options = {
+  var server = httpServer.createServer({
     proxy: argv['proxy'],
     ssl: argv['ssl']
-  }
-  
-  var server = httpServer.createServer(options);
-  server.listen(port, argv['host'], function () {
-    var canonicalHost = argv['host'] === '0.0.0.0' ? '127.0.0.1' : argv['host'],
-        protocol      = argv['ssl'] ? 'https://' : 'http://';
+  });
 
-    logger.info(['Starting up http-server, serving '.yellow,
+  server.listen(port, argv['host'], function () {
+    var canonicalHost = argv['host'] === '0.0.0.0' ? '127.0.0.1' : argv['host'];  //若host是域名形式，则必须在系统host文件中指向127.0.0.1
+    var protocol      = argv['ssl'] ? 'https://' : 'http://';
+
+    //打印 http server 启动消息
+    logger.info([
+      'Starting up http-server, serving '.yellow,
       server.root.cyan,
       argv['ssl'] ? (' through'.yellow + ' https'.cyan) : '',
       '\nAvailable on:'.yellow
     ].join(''));
 
+    //打印可用 url
     if (argv['host'] !== '0.0.0.0') {
-      logger.info(('  ' + protocol + canonicalHost + ':' + port.toString()).green);
+      logger.info((protocol + canonicalHost + ':' + port.toString()).green);
     } else {
       var ifaces = os.networkInterfaces();
       Object.keys(ifaces).forEach(function (dev) {
         ifaces[dev].forEach(function (details) {
           if (details.family === 'IPv4') {
-            logger.info(('  ' + protocol + details.address + ':' + port.toString()).green);
+            logger.info((protocol + details.address + ':' + port.toString()).green);
           }
         })
       })
     }
 
+    //启用代理
     if (argv['proxy'] && typeof argv['proxy'] === 'string') {
       logger.info('Unhandled requests will be served from: ' + argv['proxy']);
     }
+
     logger.info('Hit CTRL-C to stop the server');
-    if (argv.o) {
+
+    //自动在浏览器中打开首页地址
+    if(argv.o) {
       opener(
         protocol + '//' + canonicalHost + ':' + port,
-        { command: argv.o !== true ? argv.o : null }
-      );
+        {
+          command: argv.o !== true ? argv.o : null
+        }
+      )
     }
-  });
+  })
 }
 
 if (process.platform === 'win32') {
